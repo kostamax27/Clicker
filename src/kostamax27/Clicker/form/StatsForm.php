5cb1041e218c5a86b1a5f2ab0e2d5a0c8116ff25
kostamax27 <69608272+kostamax27@users.noreply.github.com>
2021-04-20 16:40:39 +0300

    Update StatsForm.php

--- src/kostamax27/Clicker/form/StatsForm.php
+++ src/kostamax27/Clicker/form/StatsForm.php
@@ -1,1 +1,28 @@
+<?php
 
+declare(strict_types=1);
+
+namespace kostamax27\Clicker\form;
+
+use kostamax27\Clicker\Main;
+
+use jojoe77777\FormAPI\SimpleForm;
+
+use pocketmine\Player;
+
+class StatsForm extends SimpleForm
+{
+	
+	public function __construct(string $player, array $config)
+	{
+		parent::__construct(function(Player $player, int $data = null)
+		{
+			return;
+        });
+        
+        $stats = Main::getInstance()->getStats($player);
+        $this->setTitle($config["form"]["title"]);
+        $this->setContent(str_replace(["{level}", "{xp}", "{total}", "{clicks}"], [$stats["level"], $stats["xp"], $stats["total"], $stats["clicks"]], $config["form"]["stats"]));
+        $this->addButton($config["form"]["exit"]);
+	}
+}
