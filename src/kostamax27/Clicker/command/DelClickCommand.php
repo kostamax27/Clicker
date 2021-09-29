60bf5a3b1fc21410b456ecac732da67bc0aaa5d9
kostamax27 <69608272+kostamax27@users.noreply.github.com>
2021-09-29 08:48:16 +0300

    Update DelClickCommand.php

--- src/kostamax27/Clicker/command/DelClickCommand.php
+++ src/kostamax27/Clicker/command/DelClickCommand.php
@@ -10,25 +10,19 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\Player;
 
-class DelClickCommand extends Command
-{
+class DelClickCommand extends Command {
 	
 	private $main;
 	
-	public function __construct(Main $main, string $name, string $description, string $permission)
-	{
+	public function __construct(Main $main, string $name, string $description, string $permission) {
 		$this->main = $main;
-		
 		parent::__construct($name, $description);
 		$this->setPermission($permission);
 	}
 	
-	public function execute(CommandSender $sender, string $label, array $args)
-	{
+	public function execute(CommandSender $sender, string $label, array $args) {
 		if(!$this->testPermission($sender)) return;
-		
 		if(!$sender instanceof Player) return $sender->sendMessage("§cUse command in game!");
-		
 		$this->main->getConfig()->remove("clicker_position");
 		$this->main->getConfig()->save();
 		unset($this->main->config["clicker_position"]);
