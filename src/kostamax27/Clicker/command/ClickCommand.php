c724cd82252426acbdfaaf3705acdf33b9912d36
kostamax27 <69608272+kostamax27@users.noreply.github.com>
2021-09-29 08:47:30 +0300

    Update ClickCommand.php

--- src/kostamax27/Clicker/command/ClickCommand.php
+++ src/kostamax27/Clicker/command/ClickCommand.php
@@ -11,25 +11,19 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\Player;
 
-class ClickCommand extends Command
-{
+class ClickCommand extends Command {
 	
 	private $main;
 	
-	public function __construct(Main $main, string $name, string $description, string $permission)
-	{
+	public function __construct(Main $main, string $name, string $description, string $permission) {
 		$this->main = $main;
-		
 		parent::__construct($name, $description);
 		$this->setPermission($permission);
 	}
 	
-	public function execute(CommandSender $sender, string $label, array $args)
-	{
+	public function execute(CommandSender $sender, string $label, array $args) {
 		if(!$this->testPermission($sender)) return;
-		
 		if(!$sender instanceof Player) return $sender->sendMessage("§cUse command in game!");
-		
 		$sender->sendForm(new StatsForm($sender->getName(), $this->main->config));
 	}
 }
